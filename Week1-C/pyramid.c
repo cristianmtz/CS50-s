12bc067caf7dcbdc57c9e677eaa40b18d2d0851f
cristianmtz <cristianalex0117@hotmail.com>
2024-03-13 17:23:32 -0600

    print a right-aligned pyramid file

---
+++ Week1-C/pyramid.c
@@ -0,0 +1,46 @@
+/*Print a right-aligned puramid
+
+#
+##
+###
+####
+#####
+
+author: Cristian M.
+*/
+
+
+/*---------------------Includes--------------------*/
+#include "cs50.h"
+#include <stdio.h>
+
+
+
+/*---------------------Prototype functios----------*/
+void print_row(int height);
+
+
+
+int main(void)
+{
+    
+    int height = get_int("Height: ");
+    print_row(height);
+    
+    return 0;
+}
+
+
+void print_row(int height)
+{
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
