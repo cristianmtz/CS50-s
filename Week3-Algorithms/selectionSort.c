c2f9218e104a8e3f28dd41863b8612409682647c
cristianmtz <cristianalex0117@hotmail.com>
2024-04-10 14:23:50 -0600

    Week-3 Algorithms

---
+++ Week3-Algorithms/selectionSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+
+    int array[] = {7,2,5,4,1,6,0,3};
+    int length = sizeof(array) / sizeof(array[0]);
+    printf("size: %i", length);
+    int min = 0;
+
+
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - i; j++)
+        {
+           if()
+           {
+
+           }
+        }
+        
+    }
+
+
+}
